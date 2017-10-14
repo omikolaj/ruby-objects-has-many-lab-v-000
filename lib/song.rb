b7976a92197ca80131e55c155f35d5b0d56f8ce3
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-14 20:01:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song
-  attr_accessor :artist, :name
+  attr_accessor :artist, :name, :song_count
+
+  @@song_count = []
 
   def initialize(name)
-    self.name = name
-    @@songs = []
-    @@songs << self
+    self.name = name  
+    @@song_count << self
+    
   end
 
 end
